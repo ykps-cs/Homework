00f04c2f33e6cf1fce511f05edd8c938f93dd47c
BuildTools <davidmaamoaix@gmail.com>
2022-01-18 11:20:17 +0800

    updates

---
+++ src/ykps/Testing.java
@@ -0,0 +1,10 @@
+package ykps;
+
+public class Testing {
+	
+	public static void main(String[] args) {
+		// do your testing here
+		
+		System.out.println("Testing");
+	}
+}
---
+++ src/ykps/day_1_basics/BasicNumbers.java
@@ -0,0 +1,36 @@
+package ykps.day_1_basics;
+
+public class BasicNumbers {
+
+	/**
+	 * Returns the greatest common factor of the inputed integers.
+	 */
+	public static int greatestCommonFactor(int a, int b) {
+		return 0;
+	}
+	
+	/**
+	 * Returns the least common denominator of the inputed integers.
+	 */
+	public static int leastCommonDenominator(int a, int b) {
+		return 0;
+	}
+	
+	/**
+	 * The input is an array of positive integers. The function
+	 * should return the sum of even numbers minus the sum of odd
+	 * numbers in the array.
+	 */
+	public static int strangeSum(int[] nums) {
+		return 0;
+	}
+	
+	/**
+	 * The input is an array of integers, and every integer appears
+	 * twice except for one outlier, which only appears once in the
+	 * array. Find that outlier and return it. 
+	 */
+	public static int findUnique(int[] nums) {
+		return 0;
+	}
+}
